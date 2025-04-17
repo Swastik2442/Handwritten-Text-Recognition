e2cc0dbd4e55cb19aefa3679c056e2dce62ace09
Swastik Kulshreshtha <56737344+Swastik2442@users.noreply.github.com>
2025-04-17 23:31:00 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from tempfile import NamedTemporaryFile
+from PIL import Image
+
+import torch
+from craft.create import TextRegions
+import streamlit as st
+
+# pylint: disable=unused-import
+from src.model import CRNN # Required for the Model to load
+from test_model import test_full
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = torch.load("weights/crnn_model_000.pkl", map_location=device)
+craft = TextRegions(trained_model="weights/craft_mlt_25k.pth", show_time=False)
+
+st.set_page_config("HTR App")
+st.title("🖼️ Handwritten Text Recognition")
+
+uploaded_file = st.file_uploader("Upload an Image", type=("png", "jpg", "jpeg"))
+
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+
+    if st.button("Extract Text"):
+        with st.spinner("Processing..."):
+            file = NamedTemporaryFile("wb", suffix=".png", delete=False)
+            image.save(file, "png")
+            extracted_text = test_full(model, craft, file.name)
+            file.close()
+        st.subheader("📄 Extracted Text:")
+        st.code(extracted_text, language="text")
